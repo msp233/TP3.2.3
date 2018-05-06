604d2e93f9b1899a9cb0d3f28c0d0285c2e12d28
msp233 <937207392@qq.com>
2018-05-06 23:14:57 +0800

    上传模板文件

---
+++ Application/Admin/Controller/PublicController.class.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: msp
+ * Date: 2018/5/6
+ * Time: 22:40
+ */
+namespace Admin\Controller;
+use Think\Controller;
+class PublicController extends Controller{
+    public function login(){
+        $this->display();
+    }
+}
